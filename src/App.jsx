8468d5100a58b4a310f87154494d043d3b3b9eca
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-05-26 12:25:39 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Rocket } from "./components/canvas/Rocket";
+import { OrbitControls } from "drei/OrbitControls";
 
 function App() {
-
   return (
     <>
-      <Canvas>
+      <Canvas style={{ width: 800, height: 800 }}>
+        <ambientLight intensity={2} color="#fff" />
+        <OrbitControls />
         <Rocket />
       </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
