7ef67ac797960b5d6bdaf9eb9547c4124883478c
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-05-29 15:06:18 +0200

    Add comments

--- src/components/canvas/Planet.jsx
+++ src/components/canvas/Planet.jsx
@@ -3,16 +3,21 @@ import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { useFrame, useLoader } from "@react-three/fiber";
 
 export function Planet() {
+  //Get texture for the sphere (Planet)
   const colorMap = useLoader(TextureLoader, "mars.jpg");
+  // Makes the sphere rotate
   const myMesh = React.useRef();
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime();
     myMesh.current.rotation.y = a / 3;
   });
+
   return (
     <>
+      //Lightning
       <ambientLight intensity={0.2} />
       <directionalLight />
+      // Planet
       <mesh rotation-x={-0.5} ref={myMesh}>
         <sphereGeometry args={[3, 32, 32]} />
         <meshStandardMaterial map={colorMap} />
