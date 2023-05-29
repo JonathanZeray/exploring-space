3d4321fdf25fb774ab6034bf6036ed30590ec4ee
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-05-29 11:33:12 +0200

    Added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Rocket } from "./components/canvas/Rocket";
+import { Card } from "./components/Card";
 import { OrbitControls } from "drei/OrbitControls";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Card />
       <Canvas>
         <ambientLight intensity={2} color="#fff" />
         <OrbitControls />
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+export function Card() {
+  return (
+    <div class="w-2/4">
+      <div className="bg-cover bg-gradient-to-r from-indigo-700 to-indigo-900 shadow-lg shadow-[#040c16] group container rounded-md ">
+        <h2>Mars</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, dolor
+          necessitatibus voluptatum sapiente quisquam dolorum ullam itaque sunt
+          officiis ducimus fuga cumque ratione autem recusandae pariatur
+          blanditiis ut aspernatur facere.
+        </p>
+      </div>
+    </div>
+  );
+}
