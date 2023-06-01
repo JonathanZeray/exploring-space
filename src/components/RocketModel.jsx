643401ac68609f575a8f1f72bf9f80036014873e
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-06-01 09:58:27 +0200

    Changed zoom

--- src/components/RocketModel.jsx
+++ src/components/RocketModel.jsx
@@ -14,8 +14,8 @@ export function RocketModel() {
       <ScrollControls pages={1} damping={0.4}>
         <PresentationControls
           snap // Skickar tillbaka modellen till start position oavsett hur man vridit den. 
-          // global - gör att du kan dra vart som helst på hela canvasen, utan den måste du ha musen på modellen. 
-          zoom={0.8} // högre zoom (2 ex), vid drag dras modellen närmare dig. 
+           global // - gör att du kan dra vart som helst på hela canvasen, utan den måste du ha musen på modellen. 
+          zoom={1.25} // högre zoom (2 ex), vid drag dras modellen närmare dig. 
           rotation={[0, -Math.PI / 3, 0]} 
           polar={[0, Math.PI / 4]} // Bestämmer vertikal begränsning
           azimuth={[-Math.PI / 1, Math.PI / 1]} // Horisontal begränsning
