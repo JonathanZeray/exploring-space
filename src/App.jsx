05a31300e433fce0b8addc2ecd06f7c63014a919
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-06-02 11:55:24 +0200

    Added sound effect when the rocket takes off

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { RocketModel } from "./components/RocketModel";
 import { Canvas } from "@react-three/fiber";
 import { Background } from "./components/canvas/Background";
+import { RocketSound } from "./components/RocketSound";
 
 function App() {
   return (
-    <div className="relative ">
+    <div className="relative">
       <Background />
+      <RocketSound />
       <div id="rocket">
         <RocketModel />
       </div>
