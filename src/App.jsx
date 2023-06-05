c391afa0d33dfb8eff73957222406f2cc30a24ab
Norozkhani <madeleine.norozkhanni@gmail.com>
2023-06-05 15:57:04 +0200

    Created solar system component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { Cards } from "./components/Cards";
 import { RocketModel } from "./components/RocketModel";
 import { Background } from "./components/canvas/Background";
 import { RocketSound } from "./components/RocketSound";
+import { SolarSystem } from "./components/SolarSystem";
 import "./App.css";
 
 function App() {
   return (
-    <div className="relative">
-      <Background />
-      <RocketSound />
-      <div id="rocket">
-        <RocketModel />
+    <>
+      <div className="relative">
+        <Background />
+        <RocketSound />
+        <div id="rocket">
+          <RocketModel />
+        </div>
+        <Cards />
       </div>
-      <Cards />
-    </div>
+      <SolarSystem />
+    </>
   );
 }
 
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+export function SolarSystem() {
+  return (
+    <div className="grid place-items-center w-screen my-4">
+      <iframe
+        className="relative z-20 w-9/12 h-96 "
+        src="https://solar-system-vr.vercel.app/"
+        frameborder="0"
+      ></iframe>
+    </div>
+  );
+}
