7ea37dff826d964820610482ca3a1972952e63e0
annaBohlin <anna@bohlinoffice.se>
2023-06-05 10:32:55 +0200

    make planet textures dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
-import { Canvas } from "@react-three/fiber";
-import { Planet } from "./canvas/Planet";
-import React, { useState, useEffect } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import React, { useState, useEffect, useRef } from "react";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 export function Card() {
+
+  // Makes the sphere rotate
+  // const myMesh = React.useRef();
+  // useFrame(({ clock }) => {
+  //   const a = clock.getElapsedTime();
+  //   myMesh.current.rotation.y = a / 3;
+  // });
   const [planets, setPlanets] = useState([]);
 
   useEffect(() => {
@@ -28,7 +35,14 @@ export function Card() {
         >
           <div className=" basis-1/2">
             <Canvas>
-              <Planet />
+              {/* Lightning for sphere/Planet */}
+              <ambientLight intensity={0.2} />
+              <directionalLight />
+              {/* Creating the Sphere/Planet */}
+              <mesh rotation-x={0} >
+                <sphereGeometry args={[3, 32, 32]} />
+                <meshStandardMaterial map={useLoader(TextureLoader, planet.texture)} />
+              </mesh>
             </Canvas>
           </div>
           <div className="text-white pt-6 w-2/3 bg-cover bg-gradient-to-r from-blue-950 to-slate-900 shadow-lg shadow-[#040c16] group container rounded-md basis-1/3">
